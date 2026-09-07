43e26f37b31ad2969e84a7373dc99d974cd60b18
Ahmad Naji <ahmadnajikam@gmail.com>
2026-09-07 19:39:45 +0300

    feat: make a grid with the renderer

--- renderer.py
+++ renderer.py
@@ -4,26 +4,37 @@ class Renderer:
     seed: dict[str, int] = {}
 
     def load_seed(self) -> None:
-        self.seed["rows"] = 40
-        self.seed["cols"] = 40
+        self.seed["rows"] = 21
+        self.seed["cols"] = 21
 
     def render(self) -> None:
         for r in range(int(self.seed["rows"] / 2)):
             if r == 0:
                 print("\u250F", end="")
                 for c in range(self.seed["cols"] - 2):
-                    print("\u2501", end="")
+                    if c % 2:
+                        print("\u2533", end="")
+                    else:
+                        print("\u2501", end="")
                 print("\u2513")
+
             elif r == int(self.seed["rows"] / 2) - 1:
                 print("\u2517", end="")
                 for c in range(self.seed["cols"] - 2):
-                    print("\u2501", end="")
+                    if c % 2:
+                        print("\u253B", end="")
+                    else:
+                        print("\u2501", end="")
                 print("\u251B")
+
             else:
-                print("\u2503", end="")
+                print("\u2523", end="")
                 for c in range(self.seed["cols"] - 2):
-                    print(end=" ")
-                print("\u2503")
+                    if c % 2:
+                        print("\u254B", end="")
+                    else:
+                        print(end="\u2501")
+                print("\u252B")
 
 
 def main() -> None:
