96bcfd2e684ee2351c76b977862862024c20d920
Ahmad Naji Mahmoud Kamamji <ahmadnajikam@gmail.com>
2026-09-12 14:43:30 +0300

    feat: improve the renderer borders

--- renderer.py
+++ renderer.py
@@ -1,4 +1,5 @@
 from enum import StrEnum, Enum
+from os import system
 import termios
 import sys
 import tty
@@ -50,28 +51,36 @@ class Printable(StrEnum):
 
 class Renderer:
     _corner: dict[int, Printable] = {
-        1: Printable.UVLINE, 2: Printable.RHLINE, 3: Printable.BLCORNER,
-        4: Printable.BVLINE,  5: Printable.VLINE, 6: Printable.ULCORNER,
-        7: Printable.RHCROSS, 8: Printable.LHLINE, 9: Printable.BRCORNER,
-        10: Printable.HLINE, 11: Printable.UHCROSS, 12: Printable.URCORNER,
-        13: Printable.LHCROSS, 14: Printable.BHCROSS, 15: Printable.CROSS
+        0: " ", 1: Printable.UVLINE, 2: Printable.RHLINE,
+        3: Printable.BLCORNER, 4: Printable.BVLINE,  5: Printable.VLINE,
+        6: Printable.ULCORNER, 7: Printable.RHCROSS, 8: Printable.LHLINE,
+        9: Printable.BRCORNER, 10: Printable.HLINE, 11: Printable.UHCROSS,
+        12: Printable.URCORNER, 13: Printable.LHCROSS, 14: Printable.BHCROSS,
+        15: Printable.CROSS
     }
     _maze: list[str] = []
-    _translator: dict[str, int] = {
+    _strtohex: dict[str, int] = {
         '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
         '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
         'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
 
-    def _load_seed(self) -> None:
+    def _load_maze(self) -> None:
         self._maze: list[str] = [
-            "BB913D13B",
-            "AC2AC56C2",
-            "C3EA97956",
-            "BC3AC3A97",
-            "87C696AC3",
-            "855543C52",
-            "C5557C556"
-        ]
+            "BBD7D3FD3BFBFBF",
+            "C6953A93AC1692F",
+            "D56BAAAAC3C3AC7",
+            "F93C6AC6D2BAA97",
+            "D6C552B916AC687",
+            "B9553AAEC5697AB",
+            "EC53C6C5555692E",
+            "D156955139556AF",
+            "FA93853AC6953C7",
+            "D6AC692AD3C3A97",
+            "D5453EAC56946C7",
+            "D553C785396953B",
+            "B97A916946D692E",
+            "EC3AAC3C55556C7",
+            "D7EEEFED7D7FD7F"]
 
     def _cursor_position(self) -> tuple[int, int]:
         fd = sys.stdin.fileno()
@@ -97,28 +106,28 @@ class Renderer:
         for r in range(rows + 1):
             # TODO: Needs some refactoring for the color codes
             if r == 0:
-                print("\x1B[47;37m" + Printable.ULCORNER, end="\x1B[0m")
+                print(end=Printable.ULCORNER)
                 for c in range(cols - 1):
-                    print("\x1B[47;37m" + Printable.HLINE, end="\x1B[0m")
-                print("\x1B[47;37m" + Printable.URCORNER + "\x1B[0m")
+                    print(end=Printable.HLINE)
+                print(Printable.URCORNER)
 
             elif r == int(rows):
-                print("\x1B[47;37m" + Printable.BLCORNER, end="\x1B[0m")
+                print(end=Printable.BLCORNER)
                 for c in range(cols - 1):
-                    print("\x1B[47;37m" + Printable.HLINE, end="\x1B[0m")
-                print("\x1B[47;37m" + Printable.BRCORNER + "\x1B[0m")
+                    print(end=Printable.HLINE)
+                print(Printable.BRCORNER)
 
             else:
-                print("\x1B[47;37m" + Printable.VLINE, end="\x1B[0m")
+                print(end=Printable.VLINE)
                 for c in range(cols - 1):
                     print(end=" ")
-                print("\x1B[47;37m" + Printable.VLINE + "\x1B[0m")
+                print(Printable.VLINE)
         end_coordinates: tuple[int, int] = self._cursor_position()
         return [(start_coordinates[0] + 1, start_coordinates[1]),
                 end_coordinates]
 
     def _converter(self, character: str) -> int:
-        return self._translator[character]
+        return self._strtohex[character]
 
     def _decide_corner(self, row: int, col: int) -> str:
         hex_value: int = self._converter(self._maze[row][col])
@@ -146,24 +155,33 @@ class Renderer:
                     start_coordinates[0] + 1 + row * 2,
                     start_coordinates[1] + 1 + (col * 3))
                 self._move_cursor(place)
-                if col == col_length - 1:
-                    if self._converter(self._maze[row][col]) & Directions.SOUTH:
-                        print(end=2 * Printable.HLINE)
-                    continue
-                if row == row_length - 1:
-                    if self._converter(self._maze[row][col]) & Directions.EAST:
-                        self._move_cursor((place[0] - 1, place[1] + 2))
-                        print(end=Printable.VLINE)
-                    continue
-
                 if self._converter(self._maze[row][col]) & Directions.SOUTH:
-                    print(end=2 * Printable.HLINE)
+                    if row != row_length - 1:
+                        if col == 0:
+                            self._move_cursor((place[0], place[1] - 1))
+                            print(end=Printable.RHCROSS)
+                            print(end=2 * Printable.HLINE)
+                        elif col == col_length - 1:
+                            print(end=2 * Printable.HLINE)
+                            print(end=Printable.LHCROSS)
+                        else:
+                            print(end=2 * Printable.HLINE)
                 else:
                     print(end="  ")
-                print(end=self._decide_corner(row, col))
+
+                if row != row_length - 1 and col != col_length - 1:
+                    print(end=self._decide_corner(row, col))
                 self._move_cursor((place[0] - 1, place[1] + 2))
+
                 if self._converter(self._maze[row][col]) & Directions.EAST:
                     print(end=Printable.VLINE)
+                    if col != col_length - 1:
+                        if row == 0:
+                            self._move_cursor((place[0] - 2, place[1] + 2))
+                            print(end=Printable.BHCROSS)
+                        elif row == row_length - 1:
+                            self._move_cursor((place[0], place[1] + 2))
+                            print(end=Printable.UHCROSS)
                 else:
                     print(end=" ")
 
@@ -171,18 +189,14 @@ class Renderer:
         print(end=f"\x1B[{coordinates[0]};{coordinates[1]}H")
 
     def render(self) -> None:
-        # This is the final render function
-        self._load_seed()
-        # coordinates: tuple[int, int] = self._cursor_position()
+        # Clear the terminal before drawing
+        system('clear')
+        self._load_maze()
         coordinates: list[tuple[int, int]] = self._render_borders()
         self._draw(coordinates[0])
-        # self._draw(coordinates)
 
-        # To restore the cursor to the right place
+        # Restore the cursor to the right place
         print(end=f"\x1B[{coordinates[1][0]};{coordinates[1][1]}H")
-        # print(end="\x1B7")
-        # self._draw()
-        # print(end="\x1B8")
 
 
 def main() -> None:
